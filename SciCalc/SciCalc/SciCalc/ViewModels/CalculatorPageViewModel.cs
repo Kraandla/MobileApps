bb5f25d3b2bafdecee9b31a376a8419015a02bb5
Kraandla <kevinrandla22@gmail.com>
2025-09-08 16:17:11 +0300

    Calculator page

---
+++ SciCalc/SciCalc/SciCalc/ViewModels/CalculatorPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciCalc.ViewModels
+{
+    [INotifyPropertyChanged]
+    internal class CalculatorPageViewModel
+    {
+        [ObservableProperty]
+        private string inputText = string.Empty;
+
+        [ObservableProperty]
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
+
+        [RelayCommand]
+        private void Reset()
+        {
+            calculatedResult = "0";
+            inputText = string.Empty;
+            isSciOpWaiting = false;
+        }
+
+        [RelayCommand]
+        private void Calculate()
+        {
+            if (inputText.Length == 0)
+            {
+                return;
+            }
+            if (isSciOpWaiting)
+            {
+                inputText += ")";
+                isSciOpWaiting = false;
+            }
+        }
+    }
+}
